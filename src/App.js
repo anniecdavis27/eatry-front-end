b2f3db8f62e35d0fed456fea3f5ccc7b643eb520
Annie Davis <anniecdavis27@gmail.com>
2020-07-17 15:32:13 -0400

    nav added for team page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import SignIn from './Components/SignIn'
 import Dash from './Components/Dash'
 import MealLog from './Components/MealLog'
 import Foods from './Components/Foods'
+import TheTeam from './Components/TheTeam'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Route path='/dash' component={Dash} />
       <Route path='/log' component={MealLog} />
       <Route path='/foods' component={Foods} />
+      <Route path='/team' component={TheTeam} />
       <Route path='/sign-in' component={SignIn} />
       <Route path='/' component={Welcome} />
       </Switch>
---
+++ src/Components/TheTeam.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from './Layout'
+
+function TheTeam() {
+  return (
+    <div className="meal-log">
+        <Layout>
+            <h2>Meet the Team: </h2>
+            </Layout>
+    </div>
+  );
+}
+
+export default TheTeam;
