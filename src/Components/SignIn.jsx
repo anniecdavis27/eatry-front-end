46fc8c5fd8789f6c91ff1bdcdbfde827115a1643
Annie Davis <anniecdavis27@gmail.com>
2020-07-17 17:57:49 -0400

    username/password collecting information

--- src/Components/SignIn.jsx
+++ src/Components/SignIn.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import SignInForm from '../Components/SignInForm'
 
 function SignIn() {
+
+  const [input, setInput] = useState({ name: "", urlLive: "", urlGit: "" });
+
+  const handleChange = (event) => {
+    console.log("event", event.target.name, event.target.value);
+    setInput({
+      ...input,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  console.log(input)
+
   return (
     <div className="sign-in">
       <h1>EATR.Y</h1>
-      <SignInForm />
+      <SignInForm 
+      handleChange={handleChange}
+      // handleSubmit={handleSubmit}
+      />
       <Link to='/dash'><button>Enter</button></Link>
     </div>
   );
--- src/Components/SignInForm.jsx
+++ src/Components/SignInForm.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 // import { Link } from "react-router-dom";
 
-function SignInForm() {
+function SignInForm({ input, handleChange }) {
   return (
     <div className="sign-in-form">
       <form>
         <label>Username: </label>
-        <input placeholder="Enter your username" type="text" value='' />
+        <input placeholder="Enter your username" type="text" value={input} onChange={handleChange} />
         <br />
         <label>Password: </label>
-        <input placeholder="Enter your username" type="password" value="" />
+        <input placeholder="Enter your username" type="password" value={input} onChange={handleChange} />
       </form>
     </div>
   );
